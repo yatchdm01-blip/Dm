caf8d0f280464bc5e9ab0b2f82103a00094f4f80
yatchdm01-blip <yatchdm01@gmail.com>
2026-05-11 21:25:25 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// Substitua as strings abaixo pelos valores reais do seu painel do Supabase
+const supabaseUrl = 'https://supabase.com/dashboard/project/chbpxbbfkiqoebkiycwg' 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoYnB4YmJma2lxb2Via2l5Y3dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTE1NjQsImV4cCI6MjA5NDA4NzU2NH0.n6AkkLMFCWt3gZErLP3e2MmWHzAt67-CpC4YYtFtYgQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
