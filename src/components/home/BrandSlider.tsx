2cdc15478b4fc5beca67b85cd5a6f886b8e87f5d
yatchdm01-blip <yatchdm01@gmail.com>
2026-05-24 22:27:06 +0200

    Update BrandSlider.tsx

--- src/components/home/BrandSlider.tsx
+++ src/components/home/BrandSlider.tsx
@@ -5,7 +5,6 @@ import gsap from 'gsap';
 const brands = [
   { name: 'Lewmar', logo: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/MARCA%2001.png' },
   { name: 'Lewmar', logo: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/MARCA12.png' },
-  { name: 'Lewmar', logo: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/MARCA%2001.png' },
   { name: 'Lewmar', logo: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/MARCA08.png' },
   { name: 'Lewmar', logo: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/MARCA%2011.png' },
   { name: 'Lewmar', logo: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/MARCA%2010.png' },
