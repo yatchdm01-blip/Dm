aef245850c3686a868800b91ad31dbb873adde19
yatchdm01-blip <yatchdm01@gmail.com>
2026-06-03 23:43:19 +0200

    Update InfoGrid.tsx

--- src/components/home/InfoGrid.tsx
+++ src/components/home/InfoGrid.tsx
@@ -10,7 +10,7 @@ const categories = [
   { title: '', image: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/DM%20CATAMARA%201600X1200/102.webp' },
   { title: '', image: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/BARCOS%201600X1200/100.webp' },
   { title: '', image: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/DM%20VENDA%201600X1200%20104.webp' },
-  { title: 'Deck Hardware Support', image: 'https://images.unsplash.com/photo-1516515429572-1f9f3b859894?auto=format&fit=crop&w=600&q=80' },
+  { title: '', image: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/DM%20VISTA%20DO%20ALTO%20BARCOS%201600X1200:106.webp' },
   { title: 'Safety Equipment', image: 'https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?auto=format&fit=crop&w=600&q=80' },
   { title: 'Architectural and Industrial Rigging', image: 'https://images.unsplash.com/photo-1599240813974-61041f487160?auto=format&fit=crop&w=600&q=80' },
   { title: 'Trade Terms', image: 'https://images.unsplash.com/photo-1544473403-43904d4b67bd?auto=format&fit=crop&w=600&q=80' },
