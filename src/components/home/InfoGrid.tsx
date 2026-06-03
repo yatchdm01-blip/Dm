13b970047350b16ee901cced8e846de143986ad6
yatchdm01-blip <yatchdm01@gmail.com>
2026-06-03 23:34:09 +0200

    Update InfoGrid.tsx

--- src/components/home/InfoGrid.tsx
+++ src/components/home/InfoGrid.tsx
@@ -3,7 +3,7 @@ import { motion } from 'motion/react';
 
 const categories = [
   { title: '', image: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/DM%201600X1200%20ATE%20100%20MIL.webp' },
-  { title: 'Anchor Size Guides', image: 'https://images.unsplash.com/photo-1516515429572-1f9f3b859894?auto=format&fit=crop&w=600&q=80' },
+  { title: '', image: 'https://chbpxbbfkiqoebkiycwg.supabase.co/storage/v1/object/public/DM%20yacht/DM%201600X1200%20VELEIRO.webp' },
   { title: 'Mooring Information', image: 'https://images.unsplash.com/photo-1599240813974-61041f487160?auto=format&fit=crop&w=600&q=80' },
   { title: 'Standing Rigging Assistance', image: 'https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?auto=format&fit=crop&w=600&q=80' },
   { title: 'Custom Splicing and Rigging Service', image: 'https://images.unsplash.com/photo-1544473403-43904d4b67bd?auto=format&fit=crop&w=600&q=80' },
